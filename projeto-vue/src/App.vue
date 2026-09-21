bceb05f6e695539e1d0f41b9a590095fcdf57e37
pedro <bragap379@gmail.com>
2026-09-21 07:31:45 -0300

    terceiro componente com life cycle hooks - mounted

--- projeto-vue/src/App.vue
+++ projeto-vue/src/App.vue
@@ -3,18 +3,21 @@
 <primeiroComponente />
 <lifeCycle />
 <p>propriedade: {{testando}}</p>
+<componenteTres/>
 </div>
 </template>
 
 <script>
     import primeiroComponente from "./components/primeiroComponente.vue"
     import lifeCycle from "./components/lifeCycle.vue"
+    import componenteTres from "./components/componente-tres.vue"
 
     export default {
         name: 'App',
         components: {
             primeiroComponente,
-            lifeCycle
+            lifeCycle,
+            componenteTres
         },
         data(){
             return{
---
+++ projeto-vue/src/components/componente-tres.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <p>Terceiro componente: {{ terceiro }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'componenteTres',
+    data(){
+        return{
+            terceiro: "Fazendo um terceiro componente"
+        }
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.terceiro = "Terceiro componente pelo mounted"
+        },2000)
+    }
+}
+</script>
+
+<style>
+</style>
